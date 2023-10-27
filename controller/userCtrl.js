73151f3e758d9b8bfe466b6674d13aa99dcbed7f
jaideep-bhojak <jaideep.bhojak1@gmail.com>
2023-10-27 22:05:12 +0530

    Added the get wishlist and update address func

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -59,6 +59,41 @@ const loginUserCtrl = asyncHandler( async(req, res) => {
      }
 });
 
+// admin login
+const loginAdmin = asyncHandler( async(req, res) => {
+    const {email, password} = req.body;
+    //console.log(email, password);
+    //check if user exist or not
+    const findAdmin = await User.findOne({email});
+    if(findAdmin.role !== 'admin' ) throw new Error ('Not Authorized');
+    if(findAdmin && await findAdmin.isPasswordMatched(password)){
+       //res.json(findUser);
+       const refreshToken = await generateRefreshToken(findAdmin?._id);
+       const updateUser = await User.findByIdAndUpdate(findAdmin._id, {
+           refreshToken: refreshToken 
+       },{
+           new: true
+       });
+       res.cookie('refreshToken',refreshToken,{
+           httpOnly: true,
+           maxAge: 72*60*60*1000,
+       }); 
+       res.json({
+           _id: findAdmin?._id,
+           firstname: findAdmin?.firstname,
+           lastname: findAdmin?.lastname,
+           email: findAdmin?.email,
+           mobile: findAdmin?.mobile,
+           token: generateToken(findAdmin?._id)
+       })
+
+    }else{
+       throw new Error("Invalid credentials");
+    }
+});
+
+
+
 //handle refresh token
 const handleRefreshToken = asyncHandler(async(req,res)=>{
     const cookie = req.cookies;
@@ -171,6 +206,28 @@ const updateUser = asyncHandler(async(req, res)=>{
     }
 });
 
+//save user address
+
+const saveAddress = asyncHandler(async(req, res, next)=>{
+    const { _id } = req.user;
+    validateMongoDbId(_id);
+    try {
+        const updatedUser= await User.findByIdAndUpdate(_id,{
+            address: req?.body?.address
+        }, {
+            new: true,
+        });
+
+        res.json({
+            updatedUser
+        })
+        
+    } catch (error) {
+        throw new Error(error);
+    }
+
+});
+
 const blockUser = asyncHandler(async(req, res)=>{
     const { id } = req.params;
     validateMongoDbId(id);
@@ -216,6 +273,8 @@ const updatePassword = asyncHandler(async(req, res)=>{
         res.json(user); 
     }
 });
+
+
 
 //when user clicks the forgot password button
 
@@ -263,7 +322,19 @@ const resetPassword = asyncHandler(async(req, res)=>{
     user.passwordResetExpires = undefined;
     await user.save();
     res.json(user);
-})
+});
+
+const getWishList = asyncHandler(async(req, res) => {
+    const  { _id } = req.user;
+    try {
+        const findUser = await User.findById(_id).populate('wishlist');
+        res.json(findUser);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+
 
 
 module.exports = {createUser, 
@@ -274,5 +345,8 @@ module.exports = {createUser,
     logout,
     updatePassword,
     forgotPasswordToken,
-    resetPassword
+    resetPassword,
+    loginAdmin,
+    getWishList,
+    saveAddress
     };  
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express  = require('express');
-const { createUser, loginUserCtrl, getAllUsers, getUser, deleteUser, updateUser, blockUser, unBlockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken, resetPassword } = require('../controller/userCtrl');
+const { createUser, loginUserCtrl, getAllUsers, getUser, deleteUser, updateUser, blockUser, unBlockUser, handleRefreshToken, logout, updatePassword, forgotPasswordToken, resetPassword, loginAdmin, getWishList, saveAddress } = require('../controller/userCtrl');
 const {authMiddleware, isAdmin} = require('../middlewares/authMiddleware');
 const router = express.Router();
 
@@ -9,13 +9,16 @@ router.put('/reset-password/:token', resetPassword);
 
 router.put('/password', authMiddleware, updatePassword)
 router.post('/login', loginUserCtrl);
+router.post('/admin-login', loginAdmin);
 router.get('/users', getAllUsers);
 router.get ('/refreshApi', handleRefreshToken);
 router.get ('/logout', logout);
 
+router.get('/wishlist', authMiddleware, getWishList);
 router.get('/:id', authMiddleware,isAdmin, getUser);
 router.delete('/:id', deleteUser);
 router.put('/edit-user', authMiddleware, updateUser);
+router.put('/save-address', authMiddleware, saveAddress);
 router.put('/block-user/:id', authMiddleware, isAdmin, blockUser);
 router.put('/unblock-user/:id', authMiddleware, isAdmin, unBlockUser);
 
